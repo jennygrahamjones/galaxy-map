00b420ffc92b26b8448c5bc55507e3f36371a37a
jennygrahamjones <jennygrahamjones@users.noreply.github.com>
2020-02-06 20:13:16 +0000

    Updates

---
+++ precache-manifest.170f7fd25797ef87e2523340dbfc450f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1feab8cf723acccde522e3d96a8500",
+    "url": "/galaxy-map/index.html"
+  },
+  {
+    "revision": "4c23bfc108d3f2c1f475",
+    "url": "/galaxy-map/static/css/main.4df8ec81.chunk.css"
+  },
+  {
+    "revision": "ac9f78c9f96150eb9b08",
+    "url": "/galaxy-map/static/js/2.4255cd0b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/galaxy-map/static/js/2.4255cd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c23bfc108d3f2c1f475",
+    "url": "/galaxy-map/static/js/main.f15d85ed.chunk.js"
+  },
+  {
+    "revision": "c38576b5257d8adbe1e0",
+    "url": "/galaxy-map/static/js/runtime-main.18fb0681.js"
+  }
+]);
